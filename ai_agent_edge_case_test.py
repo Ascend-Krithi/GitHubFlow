bdfcfb3b5eeb749645fcbdc90613bdd6e95025cd
gkiruthika1505 <gkiruthika1505@gmail.com>
2025-12-11 05:56:15 +0000

    Add automation scripts

---
+++ ai_agent_edge_case_test.py
@@ -0,0 +1,10 @@
+import pytest
+import time
+import logging
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from concurrent.futures import ThreadPoolExecutor
